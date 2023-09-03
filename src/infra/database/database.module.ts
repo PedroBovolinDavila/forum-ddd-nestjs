eb6a0a9120d24d87b5552f33ac9470b18d17dbba
Pedro Bovolin D' Avila <pedrodavila.bovolin@gmail.com>
2023-09-03 13:53:41 -0300

    implementing the repositories

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -17,6 +17,14 @@ import { PrismaQuestionsRepository } from './prisma/repositories/prisma-question
     PrismaQuestionCommentsRepository,
     PrismaQuestionsRepository,
   ],
-  exports: [PrismaService],
+  exports: [
+    PrismaService,
+    PrismaAnswerAttachmentsRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswersRepository,
+    PrismaQuestionAttachmentsRepository,
+    PrismaQuestionCommentsRepository,
+    PrismaQuestionsRepository,
+  ],
 })
 export class DatabaseModule {}
