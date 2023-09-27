9bec1d96175ec94f3606d0201680cb9e055021b0
Pedro Bovolin D' Avila <pedrodavila.bovolin@gmail.com>
2023-09-27 01:44:26 -0300

    upload and create attachment unit test

---
+++ src/domain/forum/application/use-cases/upload-and-create-attachment.spec.ts
@@ -0,0 +1,51 @@
+import { expect, describe, it, beforeEach } from 'vitest'
+import { InMemoryAttachmentsRepository } from 'test/repositories/in-memory-students-attachments'
+import { UploadAndCreateAttachmentUseCase } from './upload-and-create-attachment'
+import { FakeUploader } from 'test/storage/fake-uploader'
+import { InvalidAttachmentTypeError } from './errors/invalid-attachment-type-error'
+
+let inMemoryAttachmentsRepository: InMemoryAttachmentsRepository
+let fakeUploader: FakeUploader
+let sut: UploadAndCreateAttachmentUseCase
+
+describe('Upload And Create Attachment', () => {
+  beforeEach(() => {
+    inMemoryAttachmentsRepository = new InMemoryAttachmentsRepository()
+    fakeUploader = new FakeUploader()
+
+    sut = new UploadAndCreateAttachmentUseCase(
+      inMemoryAttachmentsRepository,
+      fakeUploader,
+    )
+  })
+
+  it('should be able to upload an create an attachment', async () => {
+    const result = await sut.execute({
+      fileName: 'profile.png',
+      fileType: 'image/png',
+      body: Buffer.from(''),
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(result.value).toEqual({
+      attachment: inMemoryAttachmentsRepository.items[0],
+    })
+    expect(fakeUploader.uploads).toHaveLength(1)
+    expect(fakeUploader.uploads[0]).toEqual(
+      expect.objectContaining({
+        fileName: 'profile.png',
+      }),
+    )
+  })
+
+  it('should not be able to create and register an attachment with invalid type', async () => {
+    const result = await sut.execute({
+      fileName: 'profile.png',
+      fileType: 'invalid-file-type',
+      body: Buffer.from(''),
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(InvalidAttachmentTypeError)
+  })
+})
---
+++ test/repositories/in-memory-students-attachments.ts
@@ -0,0 +1,10 @@
+import { AttachmentsRepository } from '@/domain/forum/application/repositories/attachments-repository'
+import { Attachment } from '@/domain/forum/enterprise/entities/attachment'
+
+export class InMemoryAttachmentsRepository implements AttachmentsRepository {
+  public items: Attachment[] = []
+
+  async create(attachment: Attachment): Promise<void> {
+    this.items.push(attachment)
+  }
+}
